113a7788bd7002e6b0d733ad1b92e4a1699ed062
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-05-07 10:20:06 -0500

    got database set up with migrations working

--- src/main/java/mil/swf/jm_finalproject/entity/CurrencyCode.java
+++ src/main/java/mil/swf/jm_finalproject/entity/CurrencyCode.java
@@ -1,11 +1,10 @@
 package mil.swf.jm_finalproject.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 
 @Entity
+@Table(name = "currency_code")
 public class CurrencyCode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
