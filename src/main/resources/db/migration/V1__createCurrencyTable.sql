eb4d9aaca6869ee71f150404a24eb567d98e5ed0
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-05-08 14:33:00 -0500

    reset to this version

--- src/main/resources/db/migration/V1__createCurrencyTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE currency_code
-(
-    currency_id   BIGINT NOT NULL,
-    currency_name VARCHAR(255),
-    currency_code VARCHAR(255),
-    CONSTRAINT pk_currency_code PRIMARY KEY (currency_id)
-);
