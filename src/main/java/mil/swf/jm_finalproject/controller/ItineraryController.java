d63c524a1e71833842693e54db8f78e1c3520c20
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-05-10 10:13:28 -0500

    added the global exception handler for testing, itinerary controller for get with passing test added

--- src/main/java/mil/swf/jm_finalproject/controller/ItineraryController.java
+++ src/main/java/mil/swf/jm_finalproject/controller/ItineraryController.java
@@ -3,10 +3,7 @@ package mil.swf.jm_finalproject.controller;
 import mil.swf.jm_finalproject.DTO.ItineraryDTO;
 import mil.swf.jm_finalproject.service.ItineraryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/itineraries")
@@ -21,6 +18,12 @@ public class ItineraryController {
     @PostMapping("/create")
     public ResponseEntity<ItineraryDTO> createItineraryEntry(@RequestBody ItineraryDTO itineraryDTO){
         return ResponseEntity.ok(itineraryService.createItineraryEntry(itineraryDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItineraryDTO> getItineraryById(@PathVariable Long id){
+        ItineraryDTO dto = itineraryService.getItineraryEntryById(id);
+        return ResponseEntity.ok(dto);
     }
 
 
--- src/test/java/mil/swf/jm_finalproject/controller/ItineraryControllerTest.java
+++ src/test/java/mil/swf/jm_finalproject/controller/ItineraryControllerTest.java
@@ -10,11 +10,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ItineraryController.class)
 public class ItineraryControllerTest {
@@ -61,5 +61,29 @@ public class ItineraryControllerTest {
                 .andExpect(jsonPath("$.countryId").value(65))
                 .andExpect(jsonPath("$.daysSpentInCountry").value(5))
                 .andExpect(jsonPath("$.userWantsCurrencyExchangeRate").value(false));
+    }
+
+    @Test
+    void testGetItineraryEntryById_Found() throws Exception{
+        var itineraryDTO = new ItineraryDTO(3L,4L,5L,6,true,LocalDate.of(2019,3,5),LocalDate.of(2019,3,8),3,false);
+        when(itineraryService.getItineraryEntryById(3L)).thenReturn(itineraryDTO);
+        mockMvc.perform(get("/api/itineraries/3"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(3))
+                .andExpect(jsonPath("$.userId").value(4))
+                .andExpect(jsonPath("$.countryId").value(5))
+                .andExpect(jsonPath("$.orderOnTrip").value(6))
+                .andExpect(jsonPath("$.countryOfOrigin").value(true))
+                .andExpect(jsonPath("$.startDate").value("2019-03-05"))
+                .andExpect(jsonPath("$.userWantsCurrencyExchangeRate").value(false));
+    }
+
+    @Test
+    void testGetItineraryEntryById_NotFound() throws Exception{
+        when(itineraryService.getItineraryEntryById(999L)).thenThrow(new RuntimeException("Itinerary Entry not found"));
+
+        mockMvc.perform(get("/api/itineraries/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(containsString("Itinerary Entry not found")));
     }
 }
